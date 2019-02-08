6193605e2fb19d0be161a5c0d59b6ccfe1a5e8c4
David Gasquez <davidgasquez@gmail.com>
2019-02-08 13:45:04 +0100

    :memo:

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from bson import json_util
 from google.cloud import bigquery
 import base64
 
+MONGODB_URI = os.getenv("MONGODB_URI")
+DATABASE = os.getenv("DATABASE")
+COLLECTION = os.getenv("COLLECTION")
+GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
+BIG_QUERY_TABLE = os.getenv("BIG_QUERY_TABLE")
+BIG_QUERY_DATASET = os.getenv("BIG_QUERY_DATASET")
+MAX_SKIPPED_RECORDS = int(os.getenv("MAX_SKIPPED_RECORDS", 15))
+MAX_DAYS_AGO = int(os.getenv("MAX_DAYS_AGO", 7))
+BUFFER_SIZE = int(os.getenv("BUFFER_SIZE", 50))
+
 logger = stacklogging.getLogger(__name__)
 
 
@@ -25,23 +35,12 @@ def parse_change_event(change):
     return event
 
 
-# Grab variables from the environment
-MONGODB_URI = os.getenv("MONGODB_URI")
-DATABASE = os.getenv("DATABASE")
-COLLECTION = os.getenv("COLLECTION")
-GOOGLE_CLOUD_PROJECT = os.getenv("GOOGLE_CLOUD_PROJECT")
-BIG_QUERY_TABLE = os.getenv("BIG_QUERY_TABLE")
-BIG_QUERY_DATASET = os.getenv("BIG_QUERY_DATASET")
-MAX_SKIPPED_RECORDS = int(os.getenv("MAX_SKIPPED_RECORDS", 15))
-MAX_DAYS_AGO = int(os.getenv("MAX_DAYS_AGO", 7))
-BUFFER_SIZE = int(os.getenv("BUFFER_SIZE", 50))
-
-# Connect to the desired MongoDB collection
+# Connect to MongoDB reading from secondary nodes
 mongo_client = pymongo.MongoClient(MONGODB_URI, readPreference="secondaryPreferred")
+
 db = mongo_client.get_database(DATABASE)
 col = db.get_collection(COLLECTION)
 
-# Get a resume token
 bq_client = bigquery.Client(project=GOOGLE_CLOUD_PROJECT)
 bq_dataset = bq_client.dataset(BIG_QUERY_DATASET)
 bq_table = bq_dataset.table(BIG_QUERY_TABLE)
@@ -65,3 +64,4 @@ with cursor as stream:
             for row_errors in errors:
                 for row_error in row_errors["errors"]:
                     logger.warning(row_error["message"])
+            rows_buffer = []
